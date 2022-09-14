1c39064d7ab3181191bf36c5354f6853112b8468
leeqiang250 <leeqiang250@163.com>
2022-09-15 01:22:45 +0800

    no message

--- src/test/java/AptosClientTest.java
+++ src/test/java/AptosClientTest.java
@@ -5,15 +5,13 @@ import com.aptos.utils.AptosClient;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.net.URL;
-
 public class AptosClientTest {
 
 
     private AptosClient aptosClient;
 
     @Before
-    public void setUp() throws Exception {
+    public void init() {
         aptosClient = new AptosClient("https://fullnode.devnet.aptoslabs.com");
     }
 
