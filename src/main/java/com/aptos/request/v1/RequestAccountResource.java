a39d8b2306b227426c57329b90d6cbe564e61001
leeqiang250 <leeqiang250@163.com>
2022-09-15 02:17:13 +0800

    no message

---
+++ src/main/java/com/aptos/request/v1/RequestAccountResource.java
@@ -0,0 +1,35 @@
+package com.aptos.request.v1;
+
+import com.aptos.request.IAptosRequest;
+import com.aptos.request.IAptosRequestQuery;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author liqiang
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestAccountResource implements IAptosRequest {
+
+    @Override
+    public String path() {
+        return "/v1/accounts/" + this.account + "/resource/" + this.resourceType;
+    }
+
+    String account;
+
+    String resourceType;
+
+    RequestLedgerVersionQuery query;
+
+    @Override
+    public IAptosRequestQuery query() {
+        return query;
+    }
+
+}
--- src/test/java/AptosClientTest.java
+++ src/test/java/AptosClientTest.java
@@ -1,15 +1,18 @@
+import com.alibaba.fastjson2.JSONObject;
 import com.aptos.request.v1.*;
 import com.aptos.utils.AptosClient;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
 public class AptosClientTest {
 
-
     final long ledgerVersion = 29473448L;
+
+    final String host = "https://fullnode.devnet.aptoslabs.com";
 
     final String account = "0xc73b774dd8ea3ce870a29f20e9f37bc9631198bcf21dc294cc72fea27f212a10";
 
@@ -17,7 +20,7 @@ public class AptosClientTest {
 
     @Before
     public void init() {
-        aptosClient = new AptosClient("https://fullnode.devnet.aptoslabs.com");
+        aptosClient = new AptosClient(this.host);
     }
 
     @Test
@@ -27,27 +30,56 @@ public class AptosClientTest {
                 .build();
 
         RequestBlocksByVersion requestBlocksByVersion = RequestBlocksByVersion.builder()
-                .ledgerVersion(ledgerVersion)
+                .ledgerVersion(this.ledgerVersion)
                 .query(requestBlocksByVersionQuery)
                 .build();
 
         ResponseBlocksByVersion responseBlocksByVersion = aptosClient.call(requestBlocksByVersion, ResponseBlocksByVersion.class);
-        System.out.println(responseBlocksByVersion);
+        println(responseBlocksByVersion);
     }
 
     @Test
-    public void test() {
+    public void testRequestAccountResources() {
         RequestLedgerVersionQuery requestLedgerVersionQuery = RequestLedgerVersionQuery.builder()
-                .ledgerVersion(ledgerVersion)
+                .ledgerVersion(this.ledgerVersion)
                 .build();
 
         RequestAccountResources requestAccountResources = RequestAccountResources.builder()
-                .account(account)
-                .query(requestLedgerVersionQuery)
+                .account(this.account)
+                //.query(requestLedgerVersionQuery)
                 .build();
 
         List<ResponseAccountResource> responseAccountResources = aptosClient.callList(requestAccountResources, ResponseAccountResource.class);
-        System.out.println(responseAccountResources);
+        printlnList(responseAccountResources);
+    }
+
+    @Test
+    public void testRequestAccountResource() {
+        RequestLedgerVersionQuery requestLedgerVersionQuery = RequestLedgerVersionQuery.builder()
+                .ledgerVersion(this.ledgerVersion)
+                .build();
+
+        RequestAccountResource requestAccountResources = RequestAccountResource.builder()
+                .account(this.account)
+                .resourceType("0x1::account::Account")
+                //.query(requestLedgerVersionQuery)
+                .build();
+
+        ResponseAccountResource responseAccountResource = aptosClient.call(requestAccountResources, ResponseAccountResource.class);
+        println(responseAccountResource);
+    }
+
+
+    void println(Serializable serializable) {
+        System.out.println("--------------------" + serializable.getClass().getSimpleName());
+        System.out.println(serializable);
+        System.out.println("----------------------------------------");
+    }
+
+    void printlnList(List serializable) {
+        System.out.println("--------------------" + serializable.getClass().getSimpleName());
+        System.out.println(serializable);
+        System.out.println("----------------------------------------");
     }
 
 }
