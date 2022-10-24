1ba48aaf23b8d04e180a55077a02936b2acb43c9
leeqiang250 <leeqiang250@163.com>
2022-10-24 21:01:21 +0800

    no message

--- src/main/java/com/aptos/AptosClient.java
+++ src/main/java/com/aptos/AptosClient.java
@@ -32,7 +32,7 @@ public class AptosClient extends AbstractClient {
     public AptosClient(String host, Consumer<RequestInfo> info, Consumer<String> log) {
         super(host, info, log);
 
-        this.addressPrivateKey.put("0x3564df14abd47b153746038e36f9fd98f1a7a500c977e1c3e0f29980274c5fd7", "0x4be05e389754108e42c9a3dfaa059bc65f772109c9a5faaa9e2dc5e649f179a8");
+        this.addressPrivateKey.put("0xa9e93a5297a5ee85445c52daabf0d7a8cf92f770a12e3a621690d050b2bd7e5d", "0xf976c6ed4de6580042f2b4db9cc9f5d3fc299715f5ffcddbce202b44d4b59779");
     }
 
     public Response<Account> requestAccount(String account) {
